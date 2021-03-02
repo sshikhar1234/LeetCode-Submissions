6f0dd4c33c9ed28ca44b26c29ddee04df3dbb05c
Shikhar Shah <sshikharshah@gmail.com>
2021-03-02 00:39:40 -0500

    Update goal-parser-interpretation.js

--- goal-parser-interpretation/goal-parser-interpretation.js
+++ goal-parser-interpretation/goal-parser-interpretation.js
@@ -4,5 +4,7 @@
  */
 var interpret = function(command) {
     return command.replace(/\(al\)/g,'al').replace(/\(\)/g,'o')
-
 };
+
+// \ is used for escaping special charecters.
+// / is used for defining a literal.
