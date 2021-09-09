c6c5c10eaf777e69cc4c0e476a551ae0d6c5de3a
Shikhar Shah <sshikharshah@gmail.com>
2021-09-09 12:39:45 -0400

    Time: 135 ms (30.17%), Space: 41.6 MB (12.20%) - LeetHub

---
+++ two-sum/two-sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+    var hash = new Map();
+    
+    for(var i = 0; i<nums.length;i++){
+        hash.set(nums[i],i)
+    }
+    for(var i = 0; i<nums.length;i++){
+        var targetPt = target - nums[i]
+        if(hash.has(targetPt)){
+        var j = hash.get(targetPt)
+        if(i!=j){
+            var arr = new Array();
+            arr.push(i)
+            arr.push(j)
+            return arr            
+        }
+        }
+    }
+    return null
+    
+}
