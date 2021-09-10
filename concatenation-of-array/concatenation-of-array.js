8fa076f3e7086571ea101b0fcbf065a80f50cd34
Shikhar Shah <sshikharshah@gmail.com>
2021-09-10 15:20:27 -0400

    Update concatenation-of-array.js

--- concatenation-of-array/concatenation-of-array.js
+++ concatenation-of-array/concatenation-of-array.js
@@ -3,6 +3,10 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
+    
+    //Optimal Solution
+    //    return [...nums,...nums]
+
     let ans = []
     for(let i = 0;i<nums.length;i++){
         ans.push(nums[i])
