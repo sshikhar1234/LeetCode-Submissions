fd203b671e289492c77d2e181e8a5c3975adcceb
Shikhar Shah <sshikharshah@gmail.com>
2021-09-16 01:24:06 -0400

    Update best-time-to-buy-and-sell-stock.js

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,19 +3,25 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+//     //Optimal Solution:
+//     let min = Number.MAX_VALUE
+//     let max = 0
+//    for(let i = 0;i<prices.length;i++){
+//        if(prices[i]<min){
+//            min = prices[i]
+//        }else if (prices[i] - min >max){
+//            max = prices[i] - min
+//        }
+//    }
+//     return max
     let min = Number.MAX_VALUE
     let max = 0
    for(let i = 0;i<prices.length;i++){
-       if(prices[i]<min){
-           min = prices[i]
-       }else if (prices[i] - min >max){
-           max = prices[i] - min
+       for(let j = i+1;j<prices.length;j++){
+           if(prices[j]-prices[i] > max){
+               max = prices[j]-prices[i]
+           }
        }
-       // for(let j = i+1;j<prices.length;j++){
-       //     if(prices[j]-prices[i] > max){
-       //         max = prices[j]-prices[i]
-       //     }
-       // }
    }
     return max
    
