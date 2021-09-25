af6d796c0c450b6815580aa0beab1a492274b7a0
Shikhar Shah <sshikharshah@gmail.com>
2021-09-25 16:06:54 -0400

    Time: 7 ms (28.82%), Space: 39 MB (45.53%) - LeetHub

--- reformat-phone-number/reformat-phone-number.java
+++ reformat-phone-number/reformat-phone-number.java
@@ -1,45 +1,53 @@
 class Solution {
     public String reformatNumber(String number) {
-     number = number.replace(" ","").replace("-","");
+        number = number.replace(" ","").replace("-","");
+        StringBuilder sb = new StringBuilder();
         List<String> blocks = new ArrayList<String>();
-        String res = "";
-        char[] arr = number.toCharArray();
         while(!(number.isBlank())){
-            System.out.println(number);        
             if((number.toCharArray()).length >4){
             String temp = number.substring(0,3);
-            // String[] splited = number.split(temp,1)   
             number = number.replaceFirst(temp, "");
-             System.out.println(temp);
-             System.out.println(number);
+            sb.append(temp);
+            if(!(number.isBlank())){                    
+            sb.append("-");
+            }
             blocks.add(temp);
             }
             else if((number.toCharArray()).length == 4){
             String block1 = number.substring(0,2);
             number = number.replaceFirst(block1, "");
-            // System.out.println(block1);
             blocks.add(block1);
+            sb.append(block1);
+            if(!(number.isBlank())){                    
+            sb.append("-");
+            }
             String block2 = number.substring(0,2);
             number = number.replaceFirst(block2, "");
-            // System.out.println(block2);
             blocks.add(block2);
+            sb.append(block2);
+            if(!(number.isBlank())){                    
+            sb.append("-");
+            }
             }
             else{
             if((number.toCharArray()).length == 3){
             String temp = number.substring(0,3);
             number = number.replaceFirst(temp, "");                
-            System.out.println(temp);
-            blocks.add(temp);
+            sb.append(temp);
+            if(!(number.isBlank())){                    
+            sb.append("-");
+            }
             }
             else if((number.toCharArray()).length == 2){
-             System.out.println("In 2");               
              String temp = number.substring(0,2);
             number = number.replaceFirst(temp, "");               
-            System.out.println(number);
-            blocks.add(temp);
+            sb.append(temp);
+            if(!(number.isBlank())){                    
+            sb.append("-");
+            }
             }
             }
         }
-        return  String.join("-", blocks);
+        return  sb.toString();
     }
 }
