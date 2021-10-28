db64172ba98eea198783894d68b8ae598ffae0a6
Shikhar Shah <sshikharshah@gmail.com>
2021-10-27 22:31:34 -0400

    Time: 34 ms (68.33%), Space: 49 MB (69.35%) - LeetHub

--- daily-temperatures/daily-temperatures.java
+++ daily-temperatures/daily-temperatures.java
@@ -1,13 +1,20 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+       //1. User monotonic decrease stack
+       //2. If the stack is empty, push the first item;
+       //3. If stack is not empty, check if the temp(top of stack)is < ith temp
+       //3.1 if yes, then while stack is not empty, popuplate res[stack.peek()] values
+       //4. Return stack
+        
         Stack<Integer> stack = new Stack<>();
-        int[] res  = new int[temperatures.length];
-            for(int i = 0;i<temperatures.length;i++){
-                while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
-                     res[stack.peek()] = i - stack.pop(); 
-                }
-                stack.push(i);
+        int[] res = new int[temperatures.length];
+        for(int i = 0;i<temperatures.length;i++){
+            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
+                res[stack.peek()] = i - stack.pop();
             }
+            stack.push(i);
+        }
         return res;
+
     }
 }
