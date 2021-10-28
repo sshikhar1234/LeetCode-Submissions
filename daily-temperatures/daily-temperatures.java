c83a1f221f0bc8ad59e96a60532affeac67d9756
Shikhar Shah <sshikharshah@gmail.com>
2021-10-27 22:25:08 -0400

    Time: 34 ms (68.33%), Space: 48.4 MB (87.84%) - LeetHub

--- daily-temperatures/daily-temperatures.java
+++ daily-temperatures/daily-temperatures.java
@@ -1,26 +1,13 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int p1 = 0;
-        int p2 = 0;
+        Stack<Integer> stack = new Stack<>();
         int[] res  = new int[temperatures.length];
-        for(int i = 0;i<temperatures.length;i++){
-            if(i == temperatures.length - 1) res[i] =0;
-            else{
-                    int ctr = 1;
-                for(int j = i+1;j<temperatures.length;j++){
-                p1 = temperatures[i];
-                p2 = temperatures[j];
-                if(p1 < p2){
-                    res[i] += ctr;
-                    break;
-                }else{
-                    ctr++;
-                    continue;
-                }        
+            for(int i = 0;i<temperatures.length;i++){
+                while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+                     res[stack.peek()] = i - stack.pop(); 
                 }
+                stack.push(i);
             }
-            
-        }
         return res;
     }
 }
