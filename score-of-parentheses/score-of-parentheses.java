6c7a44d57df8fb0e6651f2bd56585d08ab200e52
Shikhar Shah <sshikharshah@gmail.com>
2021-10-28 23:53:02 -0400

    Time: 0 ms (100.00%), Space: 37.2 MB (64.43%) - LeetHub

--- score-of-parentheses/score-of-parentheses.java
+++ score-of-parentheses/score-of-parentheses.java
@@ -15,37 +15,6 @@ class Solution {
             }
         }
         return sum;
-//         Stack<Character> stack = new Stack<>();
-//         int globalSum = 0;
-//         boolean madePush = false;
-//         int temp  = 0;
-//         for(char ch: s.toCharArray()){
-//             System.out.println("Temp is "+temp);
-//             System.out.println("globalSum is "+globalSum);
-//             System.out.println("Stack is "+stack);
-//             System.out.println("Ch is "+ch);
-//             if(ch == '('){
-//                 stack.push(ch);
-//                 madePush = false;
-//                 temp = 0;
-                    
-//             }
-//             if(ch == ')' ){
-//                 if(madePush){
-//             System.out.println("In if condition");
-//             System.out.println("Temp is: "+temp);
-//                     //globalSum = globalSum -  += 1 << bal;
-//                     globalSum  += 1 << temp;
-//                     stack.pop();
-//                     System.out.println("Globalsum now "+globalSum);
-//                     continue;
-//                 }
-//                 stack.pop();
-//                 temp++;
-//                 globalSum +=temp;
-//                 madePush = true;
-//             }
-//         }
-//         return globalSum;
+
     }
 }
