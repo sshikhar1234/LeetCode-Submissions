1976548c4429cdde2133ab94f48fb827b91edb4c
Shikhar Shah <sshikharshah@gmail.com>
2021-11-03 23:40:55 -0400

    Time: 4 ms (51.35%), Space: 39.1 MB (48.99%) - LeetHub

--- reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,17 +1,34 @@
 class Solution {
     public String reverseParentheses(String s) {
-        Deque<StringBuilder> stack = new ArrayDeque<>();
-        // Stack<StringBuilder> stack = new Stack();
-         stack.push(new StringBuilder());                            for(int i = 0;i<s.length();i++){
-            if(s.charAt(i) == '('){
-            stack.push(new StringBuilder());                            
-            }else if(s.charAt(i) == ')'){
-                StringBuilder deeper = stack.pop().reverse();
-                stack.peek().append(deeper);
-            }else{
-                stack.peek().append(s.charAt(i));
-            }
+       // Deque<StringBuilder> stack = new ArrayDeque<>();
+        //  stack.push(new StringBuilder());                            for(int i = 0;i<s.length();i++){
+        //     if(s.charAt(i) == '('){
+        //     stack.push(new StringBuilder());                            
+        //     }else if(s.charAt(i) == ')'){
+        //         StringBuilder deeper = stack.pop().reverse();
+        //         stack.peek().append(deeper);
+        //     }else{
+        //         stack.peek().append(s.charAt(i));
+        //     }
+        // }
+        // return stack.pop().toString();
+        Stack<Character> stack = new Stack();
+        for(int i = 0;i<s.length();i++){       
+            if(s.charAt(i) ==')'){
+                StringBuilder sb = new StringBuilder();
+                 while(stack.peek() != '(') {
+                    sb.append(stack.pop());
+                }
+                stack.pop();
+                for(int j = 0;j<sb.length();j++){                  
+                    stack.push(sb.charAt(j));
+                }
+            }else stack.push(s.charAt(i));
         }
-        return stack.pop().toString();
+        StringBuilder result = new StringBuilder();
+        while(!stack.isEmpty()) {
+            result.insert(0, stack.pop());
+        }
+        return result.toString();
     }
 }
